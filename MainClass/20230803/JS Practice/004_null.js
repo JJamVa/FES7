204db77a61b34294f1531e9d763a265b7acd836a
Jun Hong Park <hongi97@naver.com>
2023-08-04 23:41:32 +0900

    Cool clone coding project setting

--- MainClass/20230803/JS Practice/004_null.js
+++ MainClass/20230803/JS Practice/004_null.js
@@ -17,11 +17,11 @@ console.log(value2); //null
 // 반평균
 
 
-// let 반점수 = [10, 20, 30, undefined, 40, 50]
-// let 반평균 = 0
-// for (const i of 반점수) {
-//     console.log(i)
-//     반평균 += i
-// }
+let 반점수 = [10, 20, 30, null, 40, 50]
+let 반평균 = 0
+for (const i of 반점수) {
+    console.log(i)
+    반평균 += i
+}
 
 // 반평균 // NaN
