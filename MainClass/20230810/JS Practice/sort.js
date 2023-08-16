3eddaf0ee16fde3d1bce3ca94d14c1e654f0e095
Jun Hong Park <hongi97@naver.com>
2023-08-16 20:51:44 +0900

    20230816 JS Code

--- MainClass/20230810/JS Practice/sort.js
+++ MainClass/20230810/JS Practice/sort.js
@@ -1,48 +1,48 @@
-const student = [
-    {
-        id: 1, product: '연필', stock: 10
-    },
-    {
-        id: 2, product: '지우개', stock: 33
-    },
-    {
-        id: 3, product: '체육복', stock: 2
-    },
-    {
-        id: 4, product: '만연필', stock: 5
-    },
-    {
-        id: 5, product: '책받침', stock: 100
-    }
-];
+        const student = [
+            {
+                id: 1, product: '연필', stock: 10
+            },
+            {
+                id: 2, product: '지우개', stock: 33
+            },
+            {
+                id: 3, product: '체육복', stock: 2
+            },
+            {
+                id: 4, product: '만연필', stock: 5
+            },
+            {
+                id: 5, product: '책받침', stock: 100
+            }
+        ];
 
-student.sort(function (a, b) {
-    if (a.product < b.product) {
-        return -1
-    } else if (a.product > b.product) {
-        return true
-    } else {
-        return false
-    }
-});
+        student.sort(function (a, b) {
+            if (a.product < b.product) {
+                return -1
+            } else if (a.product > b.product) {
+                return true
+            } else {
+                return false
+            }
+        });
 
-// student.sort();
+        // student.sort();
 
-const numbers = [1, 4, 2, 3, 5, 6];
-numbers.sort(function (a, b) {
-    return a - b;
-});
+        const numbers = [1, 4, 2, 3, 5, 6];
+        numbers.sort(function (a, b) {
+            return a - b;
+        });
 
-// const avengers = ['spiderman', 'ironman', 'hulk', 'thor'];
-// avengers.sort();
-// avengers.sort(function (a, b) {
-//     // return a - b
-//     console.log(a, b);
-//     if (a < b) {
-//         return -1;
-//     } else if (b < a) {
-//         return 1;
-//     } else {
-//         return 0;
-//     }
-// });
+        const avengers = ['spiderman', 'ironman', 'hulk', 'thor'];
+        avengers.sort();
+        avengers.sort(function (a, b) {
+            // return a - b
+            console.log(a, b);
+            if (a < b) {
+                return -1;
+            } else if (b < a) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
